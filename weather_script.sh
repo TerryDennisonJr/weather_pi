e8b8366bcc545318359d3ddae0743d58e254e693
TerryDennisonJr <77395950+TerryDennisonJr@users.noreply.github.com>
2023-03-01 12:43:17 -0700

    Update weather_script.sh

--- weather_script.sh
+++ weather_script.sh
@@ -14,3 +14,5 @@ sudo apt install mysql-server -y
 sudo mysql --user=$user --execute="CREATE DATABASE $database; USE $database; CREATE TABLE weather_table (temp INT(3), humidity INT(3), pressure INT(3)); "
 
 sudo mysql --user=$user --execute="CREATE USER 'weather_user'@'localhost' IDENTIFIED BY ''; GRANT ALL ON *.* TO 'weather_user'@'localhost'; FLUSH PRIVILEGES;"
+
+eval "$(curl -o tester.py  https://raw.githubusercontent.com/TerryDennisonJr/weather_pi/main/weather_app_mysql.py)"

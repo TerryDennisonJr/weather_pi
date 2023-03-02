b7848b2089f5cb3f27e75d18f3605b062f58817a
TerryDennisonJr <77395950+TerryDennisonJr@users.noreply.github.com>
2023-03-01 18:05:53 -0700

    Update weather_script.sh

--- weather_script.sh
+++ weather_script.sh
@@ -11,7 +11,7 @@ pip install mysql-connector-python
 echo -e "mariadb install"
 sudo apt-get install mariadb-server
 
-sudo mysql --user=$user --execute="CREATE DATABASE $database; USE $database; CREATE TABLE weather_table (temp INT(3), humidity INT(3), pressure INT(3)); "
+sudo mysql --user=$user --execute="CREATE DATABASE $database; USE $database; CREATE TABLE weather_table (date DATE, temp INT(3), humidity INT(3), pressure INT(3)); "
 
 sudo mysql --user=$user --execute="CREATE USER 'weather_user'@'localhost' IDENTIFIED BY ''; GRANT ALL ON *.* TO 'weather_user'@'localhost'; FLUSH PRIVILEGES;"
 
